a96fea94266d63eb1af38abb788efa4dfcdbda29
Aidan Dunlop <aidandunlop@gmail.com>
2018-04-29 10:57:39 +0100

    started using spotify web api

--- components/screens/DiscoverScreen.js
+++ components/screens/DiscoverScreen.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View, Image, Button } from 'react-native';
 
 export default class DiscoverScreen extends Component {
+  state = {
+    data: ''
+  }
+  componentDidMount() {
+    fetch('https://api.spotify.com/v1/search?q=stevie%20wonder&type=track', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer BQBkv20v2Am2WgtSClTLmmPJHeSjMsrw7JEq0Ft-Qje75Tg0cuCXCC7MINUvhGgspuv6t6-guI45dv8i6Or5DDIfvmtbSOT2o1x-rlcC5c184-6dGnedq-g_APvdaSsgF09m4irnFf6ELSU'
+      },
+    })
+    .then((response) => response.json())
+      .then((responseJson) => {
+         console.log(responseJson);
+         
+         this.setState({
+            data: responseJson
+         })
+      })
+      .catch((error) => {
+         console.error(error);
+      });
+   }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ScrollView centerContent={true}>
         <Text>Discover!</Text>
-      </View>
+        { this.state.data.tracks && this.state.data.tracks.items.map((item, index) => {
+            return(
+              <View key={item.id}>
+                <Text>
+                  {item.name}
+                </Text>
+                <Image style={{width: 250, height: 250}} source={{uri: item.album.images[0].url}}/>
+                <Text>
+                  {item.preview_url}
+                  </Text>
+            </View>
+          )
+        })}
+      </ScrollView>
+        
     );
   }
 }
